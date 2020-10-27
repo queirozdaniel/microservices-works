48c334eb03b84170d0db1f3b52375eae947192f6
queirozdaniel <danielqueirozak77@gmail.com>
2020-10-26 22:01:54 -0300

    Buscando dados de trabalhador com RestTemplate

--- hr-payroll/src/main/java/com/danielqueiroz/hrpayroll/service/PaymentService.java
+++ hr-payroll/src/main/java/com/danielqueiroz/hrpayroll/service/PaymentService.java
@@ -1,16 +1,31 @@
 package com.danielqueiroz.hrpayroll.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.danielqueiroz.hrpayroll.model.Payment;
+import com.danielqueiroz.hrpayroll.model.Worker;
 
 @Service
 public class PaymentService {
 
+	@Value("${hr-worker.host}")
+	private String workerHost;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
 	public Payment getPayment(long workerId, int days) {
-		// TODO: Buscar dados de trabalhador em Serviço Worker
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("id", workerHost);
 		
-		return new Payment("Bob", 200.0, days);
+		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}",Worker.class, uriVariables);
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 	
 }

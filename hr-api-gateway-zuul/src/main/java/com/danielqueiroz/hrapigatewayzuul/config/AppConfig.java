f7fd415ecc922f85a2a7ff5c9110f894a69a060f
queirozdaniel <danielqueirozak77@gmail.com>
2020-11-01 18:54:07 -0300

    Colocando configurações de segurança em ConfigServer

--- hr-api-gateway-zuul/src/main/java/com/danielqueiroz/hrapigatewayzuul/config/AppConfig.java
+++ hr-api-gateway-zuul/src/main/java/com/danielqueiroz/hrapigatewayzuul/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.danielqueiroz.hrapigatewayzuul.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
@@ -8,10 +9,13 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @Configuration
 public class AppConfig {
 
+	@Value("${jwt.secret}")
+	private String secretKey;
+	
 	@Bean
 	public JwtAccessTokenConverter accessTokenConverter() {
 		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
-		tokenConverter.setSigningKey("MySecretKey");
+		tokenConverter.setSigningKey(secretKey);
 		return tokenConverter;
 	}
 	

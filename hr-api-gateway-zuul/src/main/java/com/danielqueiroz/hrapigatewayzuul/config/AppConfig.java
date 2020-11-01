1a6cca240c7bc4e7d479b9050feb3a0b7620f574
queirozdaniel <danielqueirozak77@gmail.com>
2020-11-01 15:42:44 -0300

    Usando autenticação em endpoints pelo Zuul

---
+++ hr-api-gateway-zuul/src/main/java/com/danielqueiroz/hrapigatewayzuul/config/AppConfig.java
@@ -0,0 +1,24 @@
+package com.danielqueiroz.hrapigatewayzuul.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+public class AppConfig {
+
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+		tokenConverter.setSigningKey("MySecretKey");
+		return tokenConverter;
+	}
+	
+	@Bean
+	public JwtTokenStore tokenStore() {
+		return new JwtTokenStore(accessTokenConverter());
+	}
+	
+	
+}
